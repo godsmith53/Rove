77de66a95b557b58fb1657fc3402cd3092e72b6b
Priyanshu Kumar <priyanshu@Priyanshus-MacBook-Air.local>
2023-08-12 22:54:57 +0530

    trash

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,15 +1,125 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:rove/utils/colors.dart';
 
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyWidget> createState() => _MyWidgetState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyWidgetState extends State<MyWidget> {
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+      body: SafeArea(
+        child: Column(
+          children: [ 
+           const Text(
+            "hell"
+            ),
+           const SizedBox(
+            height: 10,
+          ),
+          Container(
+
+                height: 192,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: const Color.fromARGB(255, 2, 1, 1))),
+           const SizedBox(
+            height: 10,
+          ),
+          Container(
+            width: 390,
+                height: 192,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: const Color.fromARGB(255, 255, 255, 255),
+                    border: Border.all(
+                      color: keyColor,
+                    )
+              ),
+              child: const Column(children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(Icons.circle,),
+                    Text("UCER",
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                            color: keyColor)),
+                    Text("R2",
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                            color: primaryColor)
+                      )
+                  ],
+                ),
+
+                Row(
+                children: [
+                    VerticalDivider(
+                      color: Colors.black,
+                      thickness: 2,
+                    ),
+                    Text("ga"),
+                ],
+                ),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(Icons.circle),
+                    Text("Shantipuram",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w600,color: keyColor )),
+                    Text("ETA - 07:50 a.m.",
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                            color: primaryColor)),
+                  ],
+                ),
+
+                Divider(
+                  height: 40,
+                  color: keyColor,
+                  thickness: 1,
+                  indent: 25.0,
+                  endIndent: 25.0,
+                  
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Bus will be there in",
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                            color: primaryColor)),
+                    Text("20 mins",
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                            color: keyColor))
+                  ],
+                ),
+                Divider(
+                  height: 40,
+                  color: keyColor,
+                  thickness: 1,
+                  indent: 25.0,
+                  endIndent: 25.0,
+                ),
+
+              ]),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/utils/colors.dart
@@ -0,0 +1,5 @@
+import 'dart:ui';
+
+  const Color keyColor = Color(0xFF292929);
+  const Color primaryColor = Color(0xFF898989);
+  const Color secondaryColor = Color(0xFFE6E6E6);
